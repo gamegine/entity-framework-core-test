499e6802c29fa1dfea985df380ceebf1b7c141fb
gamegine <gamegine@protonmail.com>
2020-04-02 14:04:26 +0200

    init EntityFrameworkCore

---
+++ entity-framework-core-test/Data/TestBddContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace entity_framework_core_test.Data
+{
+    public class TestBddContext : DbContext
+    {
+        public TestBddContext(DbContextOptions<TestBddContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Models.A> A { get; set; }
+    }
+}
---
+++ entity-framework-core-test/Models/A.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace entity_framework_core_test.Models
+{
+    public class A
+    {
+        public int Id { get; set; }
+        public string AData { get; set; }
+    }
+}
